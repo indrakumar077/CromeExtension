e1c6e7393fe3e6bf57939b7578b183dc81cf1ea6
indrakumar077 <84073920+indrakumar077@users.noreply.github.com>
2022-09-29 23:38:59 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+
+let color = 'red'
+
+chrome.runtime.onInstalled.addListener(()=>{
+
+    chrome.storage.sync.set({color});
+})
---
+++ popup.js
@@ -0,0 +1,54 @@
+
+
+const btn = document.querySelector(".changeColorBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
+
+btn.addEventListener('click', async()=>{
+
+    const color = chrome.storage.sync.get('color',({color})=>{
+
+        
+    })
+   
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+     
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: pickColor,
+    },async(injectionResults)=>{
+         
+        const [data] = injectionResults;
+        if(data.result){
+            const color = data.result.sRGBHex;
+            colorGrid.style.backgroundColor = color;
+            colorValue.innerText = color;
+            try {
+                
+                await navigator.clipboard.writeText(color);
+
+
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
+        else{
+            console.log("No");
+        }
+    })
+});
+
+
+async function pickColor(){
+
+    try {
+        
+      const eyeDropper = new EyeDropper();
+      return  await eyeDropper.open();
+      
+
+    } catch (e) {
+        console.error(e);
+    }
+}
